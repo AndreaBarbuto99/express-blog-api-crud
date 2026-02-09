640956986be1ed7d965caf03e30ef47b538de1c2
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-02-09 17:16:21 +0100

    feat: added routing in postsRouter.js with CURD from myController.js

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// import router
+
+const postsRouter = require("./routers/postsRouter");
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.send("<h1>Homepage della pagina</h1>")
 })
+
+app.use("/posts", postsRouter);
 
 
 app.listen(port, () => {
